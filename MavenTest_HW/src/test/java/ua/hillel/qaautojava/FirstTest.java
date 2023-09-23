ed9fd529abde4fb47ff74b0b01113576f252c051
dmytro_kyrylenko <sowhattzz@gmail.com>
2023-09-23 14:56:10 -0700

    homework

---
+++ MavenTest_HW/src/test/java/ua/hillel/qaautojava/FirstTest.java
@@ -0,0 +1,11 @@
+package ua.hillel.qaautojava;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void Sout() {
+        System.out.println("Homework6");
+    }
+}
