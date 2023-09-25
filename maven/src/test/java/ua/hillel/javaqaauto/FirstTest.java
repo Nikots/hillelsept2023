a0f470a21886cff56f7dc9552c707676a39decca
HARLIE\Harlie <k.teslenko@xmlshop.com>
2023-09-25 15:51:52 +0300

    add test

---
+++ maven/src/test/java/ua/hillel/javaqaauto/FirstTest.java
@@ -0,0 +1,10 @@
+package ua.hillel.javaqaauto;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+   @Test
+    public void testSout(){
+        System.out.println("First test");
+    }
+}
