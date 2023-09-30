1f59a5af31f6b1b60594e6eff5c7da4ea2f84719
ArtyukhNatalia <natalia.artyukh.01@gmail.com>
2023-09-30 13:47:56 +0300

    Add first test

---
+++ maven/src/test/java/ua/hillel/qaautojava/FirstTest.java
@@ -0,0 +1,17 @@
+package ua.hillel.qaautojava;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    @Test
+    public void testSuit() {
+        System.out.println("First test");
+
+    }
+
+
+    @Test
+    public void testSuit2() {
+        System.out.println("Second test");
+    }
+}
