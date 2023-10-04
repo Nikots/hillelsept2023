5bc7bdf3bbfc1c3fdfaf70bac38f176015643b8f
Nina Kotsar <kotsarnina@gmail.com>
2023-10-05 00:13:09 +0300

    test2

--- hometask000/src/main/java/org/example/Main2.java
+++ hometask000/src/main/java/org/example/Main2.java
@@ -4,5 +4,7 @@ public class Main2 {
     public static void main(String[] args) {
 
         System.out.println("Hello main2");
+        System.out.println("Hello main2");
+        System.out.println("Hello main2");
     }
 }
