3c6723815c9226f2f2587957211ba781622c18f9
Nina Kotsar <kotsarnina@gmail.com>
2023-10-05 00:24:27 +0300

    test7

--- hometask000/src/main/java/org/example/Main2.java
+++ hometask000/src/main/java/org/example/Main2.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main2 {
     public static void main(String[] args) {
+        System.out.println("today is ");
         System.out.println("beautiful day");
     }
 }
