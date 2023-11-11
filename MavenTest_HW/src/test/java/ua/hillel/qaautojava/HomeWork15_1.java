8c3708b0f8fa637409c8e985c40264130610f138
dmytro_kyr <sowhattzz@gmail.com>
2023-11-11 13:47:47 +0200

    homework

--- MavenTest_HW/src/test/java/ua/hillel/qaautojava/HomeWork15_1.java
+++ MavenTest_HW/src/test/java/ua/hillel/qaautojava/HomeWork15_1.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class HomeWork15_1 {
 
     @Test
@@ -17,25 +20,31 @@ public class HomeWork15_1 {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.bstackdemo.com/");
+        String[] items = {"iPhone 12", "iPhone 12 Mini"};
+        List<WebElement> elements = driver.findElements(By.className("shelf-item__title"));
+        for (int i = 0; i < elements.size(); i++) {
+            String name = elements.get(i).getText();
+            List list = Arrays.asList(items);
+            if (list.contains(name)) {
+                driver.findElements(By.className("shelf-item__buy-btn")).get(i).click();
+                continue;
+            }
+            WebElement checkout = driver.findElement(By.className("buy-btn"));
+            WebElement bag = driver.findElement(By.className("float-cart__shelf-container"));
+            Assert.assertTrue(checkout.isEnabled());
+            Assert.assertTrue(bag.getText().contains("iPhone 12\n" +
+                    "Apple\n" +
+                    "Quantity: 1\n" +
+                    "$ 799.00\n" +
+                    "-+\n" +
+                    "iPhone 12 Mini\n" +
+                    "Apple\n" +
+                    "Quantity: 1\n" +
+                    "$ 699.00\n" +
+                    "-+"), "Another items");
 
-        WebElement iphone12 = driver.findElement(By.xpath("//*[@id=\"1\"]/div[4]"));
-        iphone12.click();
-        WebElement iphone12mini = driver.findElement(By.xpath("//*[@id=\"2\"]/div[4]"));
-        iphone12mini.click();
-        WebElement checkout = driver.findElement(By.className("buy-btn"));
-        WebElement bag = driver.findElement(By.className("float-cart__shelf-container"));
-        Assert.assertTrue(checkout.isEnabled());
-        Assert.assertTrue(bag.getText().contains("iPhone 12\n" +
-                "Apple\n" +
-                "Quantity: 1\n" +
-                "$ 799.00\n" +
-                "-+\n" +
-                "iPhone 12 Mini\n" +
-                "Apple\n" +
-                "Quantity: 1\n" +
-                "$ 699.00\n" +
-                "-+"), "Another items");
 
+        }
         driver.quit();
     }
 }
